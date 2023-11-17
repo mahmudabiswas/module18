3f38d4951d352685726bd1866a4afa2bceb50395
unknown <biswasmahmuda5@gmail.com>
2023-11-16 18:28:26 -0800

    even odd number

---
+++ even.js
@@ -0,0 +1,8 @@
+let even = 1; //loop variable
+
+while (even <= 10) {
+  //loop condition
+  console.log("even number"); //loop body
+  console.log(even);
+  even = even + 2; //loop er variable change
+}
