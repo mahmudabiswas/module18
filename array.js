82874e8550ca7430defcab1d927650ea41b71473
unknown <biswasmahmuda5@gmail.com>
2023-11-16 19:51:19 -0800

    break element add in for loop

--- array.js
+++ array.js
@@ -1,7 +1,15 @@
-const number = [10, 34, 33, 23, 345, 45, 66, 76, 4, 6, 54, 454, 45];
+// const number = [10, 34, 33, 23, 345, 45, 66, 76, 4, 6, 54, 454, 45];
 
-for (let i = 0; i <= number.length; i++) {
-  console.log(number[i]);
+// for (let i = 0; i <= number.length; i++) {
+//   console.log(number[i]);
 
-  //   console.log(number);
+//   //   console.log(number);
+// }
+
+let text = ["pen", "pencil", "kolom", "khata", "jira", "mosla"];
+for (let i = 0; i <= text.length; i++) {
+  console.log(text[i]);
+  if (text[i] === "kolom") {
+    break;
+  }
 }
