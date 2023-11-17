872637bc14a6454ced66e880794c92ad33859944
unknown <biswasmahmuda5@gmail.com>
2023-11-16 16:55:51 -0800

    loopProblem solve

---
+++ loopProblem.js
@@ -0,0 +1,17 @@
+// const tablesItems = ["water", "sunglass", "pen", "penchil", "fish", "furit"];
+// const indexType = tablesItems.indexOf("pen");
+// console.log(indexType);
+
+// if (tablesItems > 4) {
+//   console.log("many items");
+// } else if (tablesItems[3] === "pen") {
+//   console.log("write way");
+// } else {
+//   return tablesItems;
+// }
+
+// let rostGiven = 0;
+// while (rostGiven < 7) {
+//   console.log("please rost den");
+//   rostGiven++;
+// }
