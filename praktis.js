32d87cae35b393ed17da3d482fcfb49f7dee7bd8
unknown <biswasmahmuda5@gmail.com>
2023-11-17 08:29:31 -0800

    odd number create

---
+++ praktis.js
@@ -0,0 +1,12 @@
+// for (let i = 0; i <= 39; i++) {
+//   console.log("aj amr mon valo nai", i);
+// }
+
+// let number = 58;
+// while (number <= 98) {
+//   console.log(number);
+//   number++;
+// }
+// for (let i = 412; i <= 456; i = i + 2) {
+//   console.log(i);
+// }
