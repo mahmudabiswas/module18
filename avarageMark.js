4dc0ad2072be980454262889b2a700ce544a8218
unknown <biswasmahmuda5@gmail.com>
2023-11-16 20:10:59 -0800

    total subject get avarage mark

---
+++ avarageMark.js
@@ -0,0 +1,8 @@
+const b = 71.3;
+const e = 34;
+const bio = 50;
+const che = 77;
+const totalSubject = b + e + bio + che;
+console.log(totalSubject);
+const avargeMark = totalSubject / 4;
+console.log(avargeMark.toFixed(2));
